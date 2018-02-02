7ca8e80434a18e149d94bb67b77e8fd1c6d5720b
Lenovo y520 <894687269@qq.com>
2018-02-02 09:29:24 +0800

    更改类名

--- frog.front/src/main/java/top/qw/front/FrogCalcultorController.java
+++ frog.front/src/main/java/top/qw/front/FrogCalcultorController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-public class IndexController {
+public class FrogCalcultorController {
 
 
     @RequestMapping("index.do")
